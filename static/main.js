15df5f86b302080c2cdbecca294d08f550e923a5
Simon Eason <sie.eason@gmail.com>
2021-05-16 21:29:35 +0800

    added dynamic option box creation

--- static/main.js
+++ static/main.js
@@ -1,3 +1,29 @@
 function change_input_fields() {
-    console.log("hello");
+    // get number of options from dropdown
+    var value = document.getElementById("options").value;
+
+    // get parent container of options div and remove child nodes
+    var container = document.getElementById("option-list")
+    while (container.hasChildNodes()) {
+        container.removeChild(container.lastChild);
+    }
+
+    // add necessary number of options
+    for (var i = 1; i <= value; i++) {
+        // create label element and set text
+        var label = document.createElement("label");
+        label.innerHTML = `Option ${i}\n`;
+
+        // create input element and append to label
+        var input = document.createElement("input");
+        input.type = "text";
+        input.name = `option${i}`;
+        label.appendChild(input);
+
+        // append label to container div
+        container.appendChild(label);
+
+        // append break to container div
+        container.appendChild(document.createElement("br"));
+    }
 }
