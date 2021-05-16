5c78dc10ac41218fd101242d654ea2f89eda9150
Simon Eason <sie.eason@gmail.com>
2021-05-16 15:09:41 +0800

    added basic test of redis updating and displaying

--- app.py
+++ app.py
@@ -6,14 +6,19 @@ con = redis.Redis(host="redis", port=6379)
 
 @app.route("/")
 def home():
-    if bottle.request.get_cookie("visited"):
-        return bottle.template("home.html", submitted=True)
-    else:
-        option_data = {"option1": con.get("option1"), "option2": con.get("option2"), "option3": con.get("option3"), "option4": con.get("option4"),}
-        return bottle.template("home.html", submitted=False, **option_data)
+    option_data = {"option1": con.get("option1") if con.get("option1") != None else 0, 
+                   "option2": con.get("option2") if con.get("option2") != None else 0, 
+                   "option3": con.get("option3") if con.get("option3") != None else 0, 
+                   "option4": con.get("option4") if con.get("option4") != None else 0}
+
+    return bottle.template("home.html", submitted=bottle.request.get_cookie("visited"), **option_data)
 
 @app.route("/", method="POST")
 def home_submit():
+    option = bottle.request.forms.options
+
+    con.incr(option)
+
     bottle.response.set_cookie("visited", "true")
     return bottle.redirect("/")
 
