3c02cf26ba9e8943481f15402300e9073bb7b658
Simon Eason <sie.eason@gmail.com>
2021-05-16 17:19:39 +0800

    new home with selecting number of options

--- app.py
+++ app.py
@@ -7,23 +7,17 @@ import string
 app = bottle.Bottle()
 con = redis.Redis(host="redis", port=6379)
 
+# show home page with number of options for poll
 @app.route("/")
 def home():
-    option_data = {"option1": con.get("option1") if con.get("option1") != None else 0, 
-                   "option2": con.get("option2") if con.get("option2") != None else 0, 
-                   "option3": con.get("option3") if con.get("option3") != None else 0, 
-                   "option4": con.get("option4") if con.get("option4") != None else 0}
+    return bottle.template("home.html")
 
-    return bottle.template("home.html", submitted=bottle.request.get_cookie("visited"), **option_data)
-
+# handle postback and redirect to appropriate options page
 @app.route("/", method="POST")
 def home_submit():
-    option = bottle.request.forms.options
+    options = bottle.request.forms.optnum
 
-    con.incr(option)
-
-    bottle.response.set_cookie("visited", "true")
-    return bottle.redirect("/")
+    return bottle.redirect(f"/create/{options}")
 
 # show creation page with number of options specified
 @app.route("/create/<options:re:[0-9]+>")
