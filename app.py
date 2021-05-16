939f67ad778f4238643b573c7b80d3f8cecc900b
Simon Eason <sie.eason@gmail.com>
2021-05-16 14:39:10 +0800

    added displaying option selection times

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ import bottle
 import redis
 
 app = bottle.Bottle()
-redis_con = redis.Redis(host="redis", port=6379)
+con = redis.Redis(host="redis", port=6379)
 
 @app.route("/")
 def home():
     if bottle.request.get_cookie("visited"):
         return bottle.template("home.html", submitted=True)
     else:
-        return bottle.template("home.html", submitted=False)
+        option_data = {"option1": con.get("option1"), "option2": con.get("option2"), "option3": con.get("option3"), "option4": con.get("option4"),}
+        return bottle.template("home.html", submitted=False, **option_data)
 
 @app.route("/", method="POST")
 def home_submit():
